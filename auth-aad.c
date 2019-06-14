7513face64f9e86098d19fba67ef15e47f0aa214
Lucas Ramage <lramage@cyberninjas.com>
2019-06-14 12:10:04 +0000

    Re-use code from Mozilla's deferred auth plugin
    Source: https://github.com/mozilla-it/openvpn_defer_auth

--- auth-aad.c
+++ auth-aad.c
@@ -6,7 +6,6 @@
  *             packet compression.
  *
  *  Copyright (C) 2002-2018 OpenVPN Inc <sales@openvpn.net>
- *  Copyright (C) 2019 CyberNinjas <info@cyberninjas.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2
@@ -22,181 +21,146 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-/*
- * This file implements a simple OpenVPN plugin module which
- * will test deferred authentication.
- *
- * Sample usage:
- *
- * setenv aad_auth 20
- * plugin plugin/auth-aad.so
- *
- * This will enable deferred authentication to occur 20
- * seconds after the normal TLS authentication process.
- *
- * See the README file for build instructions.
- */
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
 
 #include "openvpn-plugin.h"
 
-/* bool definitions */
-#define bool int
-#define true 1
-#define false 0
+/*
+ * Constants
+ */
+#define OPENVPN_PLUGIN_VERSION_MIN 3
 
 /*
  * Our context, where we keep our state.
  */
 
 struct plugin_context {
-    int aad_auth;
+    char *script_path;
 };
 
-struct plugin_per_client_context {
-    int n_calls;
-};
+void handle_sigchld(int sig)
+{
+    /*
+     * nonblocking wait (WNOHANG) for any child (-1) to come back
+     */
+    while(waitpid((pid_t)(-1), 0, WNOHANG) > 0) {}
+}
 
 /*
- * Given an environmental variable name, search
- * the envp array for its value, returning it
- * if found or NULL otherwise.
+ * Require OpenVPN Plugin API v3
  */
-static const char *get_env(const char *name, const char *envp[])
+OPENVPN_EXPORT int
+openvpn_plugin_min_version_required_v1()
 {
-    if (envp) {
-        int i;
-        const int namelen = strlen(name);
-        for (i = 0; envp[i]; ++i) {
-            if (!strncmp(envp[i], name, namelen)) {
-                const char *cp = envp[i] + namelen;
-                if (*cp == '=') {
-                    return cp + 1;
-                }
-            }
-        }
-    }
-    return NULL;
+    return OPENVPN_PLUGIN_VERSION_MIN;
 }
 
-/* used for safe printf of possible NULL strings */
-static const char *np(const char *str)
-{
-    if (str) {
-        return str;
-    } else {
-        return "[NULL]";
-    }
-}
-
-static int atoi_null0(const char *str)
-{
-    if (str) {
-        return atoi(str);
-    } else {
-        return 0;
-    }
-}
-
-OPENVPN_EXPORT openvpn_plugin_handle_t
-openvpn_plugin_open_v1(unsigned int *type_mask, const char *argv[],
-                       const char *envp[])
+OPENVPN_EXPORT int
+openvpn_plugin_open_v3(const int struct_version,
+                       struct openvpn_plugin_args_open_in const *arguments,
+                       struct openvpn_plugin_args_open_return *retptr)
 {
     struct plugin_context *context;
 
-    printf("FUNC: openvpn_plugin_open_v1\n");
+    if (struct_version < OPENVPN_PLUGINv3_STRUCTVER) {
+        return OPENVPN_PLUGIN_FUNC_ERROR;
+    }
 
     /*
      * Allocate our context
      */
-    context =
-        (struct plugin_context *) calloc(1, sizeof(struct plugin_context));
+    context = (struct plugin_context *) calloc(1, sizeof(struct plugin_context));
 
-    context->aad_auth = atoi_null0(get_env("aad_auth", envp));
-    printf("AAD_AUTH %d\n", context->aad_auth);
+    if (arguments->argv[1]) {
+        context->script_path = strdup(arguments->argv[1]);
+        if (context->script_path == NULL) {
+            perror("Unable to allocate memory\n");
+            exit(EXIT_FAILURE);
+        }
+    }
 
     /*
      * Which callbacks to intercept.
      */
-    *type_mask = OPENVPN_PLUGIN_MASK(OPENVPN_PLUGIN_AUTH_USER_PASS_VERIFY);
+    retptr->type_mask =
+        OPENVPN_PLUGIN_MASK(OPENVPN_PLUGIN_AUTH_USER_PASS_VERIFY);
 
-    return (openvpn_plugin_handle_t) context;
+    retptr->handle = (openvpn_plugin_handle_t) context;
+
+    return OPENVPN_PLUGIN_FUNC_SUCCESS;
 }
 
 static int
-auth_user_verify(struct plugin_context *context,
-                 struct plugin_per_client_context *pcc, const char *argv[],
-                 const char *envp[])
+deferred_auth_handler(struct plugin_context *context, const char *argv[], const char *envp[])
 {
-    if (context->aad_auth) {
-        /* get username from envp string array */
-        const char *username = get_env("username", envp);
+    pid_t pid;
+    struct sigaction sa;
+    char *script = context->script_path;
 
-        /* get auth_control_file filename from envp string array */
-        const char *auth_control_file = get_env("auth_control_file", envp);
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART | SA_NOCLDSTOP;
+    sa.sa_handler = &handle_sigchld;
 
-        printf("DEFER u='%s' acf='%s'\n",
-               np(username), np(auth_control_file));
-
-        /* Authenticate asynchronously in n seconds */
-        if (auth_control_file) {
-            char buf[256];
-            int auth = 2, ret;
-            sscanf(username, "%d", &auth);
-            snprintf(buf, sizeof(buf),
-                     "( sleep %d ; echo AUTH %s %d ; echo %d >%s ) &",
-                     context->aad_auth, auth_control_file, auth,
-                     pcc->n_calls < auth, auth_control_file);
-            printf("%s\n", buf);
-            ret = system(buf);
-            (void) ret;
-            pcc->n_calls++;
-            return OPENVPN_PLUGIN_FUNC_DEFERRED;
-        } else {
-            return OPENVPN_PLUGIN_FUNC_ERROR;
-        }
-    } else {
-        return OPENVPN_PLUGIN_FUNC_SUCCESS;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        return OPENVPN_PLUGIN_FUNC_ERROR;
     }
+
+    pid = fork();
+
+    if (pid < 0) {
+        /*
+         * Fork failed, bail out.
+         */
+        return OPENVPN_PLUGIN_FUNC_ERROR;
+    } else if (pid > 0) {
+        /*
+         * We're the parent.  Tell openvpn we're deferring.
+         */
+        return OPENVPN_PLUGIN_FUNC_DEFERRED;
+    } else {
+        /*
+         * We're the child.  Invoke the script.
+         */
+        execve(script, &script, (char *const*)envp);
+        /*
+         * Since we exec'ed we should never get here.  But just in case, exit hard.
+         */
+        exit(127);
+    }
+
 }
 
 OPENVPN_EXPORT int
-openvpn_plugin_func_v2(openvpn_plugin_handle_t handle,
-                       const int type,
-                       const char *argv[],
-                       const char *envp[],
-                       void *per_client_context,
-                       struct openvpn_plugin_string_list **return_list)
+openvpn_plugin_func_v3(const int struct_version,
+                       struct openvpn_plugin_args_func_in const *arguments,
+                       struct openvpn_plugin_args_func_return *retptr)
 {
-    struct plugin_context *context = (struct plugin_context *) handle;
-    struct plugin_per_client_context *pcc =
-        (struct plugin_per_client_context *) per_client_context;
-    printf("OPENVPN_PLUGIN_AUTH_USER_PASS_VERIFY\n");
-    return auth_user_verify(context, pcc, argv, envp);
+    struct plugin_context *context = (struct plugin_context *) arguments->handle;
 
+    if (struct_version < OPENVPN_PLUGINv3_STRUCTVER) {
+        return OPENVPN_PLUGIN_FUNC_ERROR;
+    }
+
+    switch (arguments->type)
+    {
+        case OPENVPN_PLUGIN_AUTH_USER_PASS_VERIFY:
+            return deferred_auth_handler(context, arguments->argv, arguments->envp);
+
+        default:
+            return OPENVPN_PLUGIN_FUNC_ERROR;
+    }
 }
 
 OPENVPN_EXPORT void
-*openvpn_plugin_client_constructor_v1(openvpn_plugin_handle_t handle)
-{
-    printf("FUNC: openvpn_plugin_client_constructor_v1\n");
-    return calloc(1, sizeof(struct plugin_per_client_context));
-}
-
-OPENVPN_EXPORT void
-openvpn_plugin_client_destructor_v1(openvpn_plugin_handle_t handle,
-                                    void *per_client_context)
-{
-    printf("FUNC: openvpn_plugin_client_destructor_v1\n");
-    free(per_client_context);
-}
-
-OPENVPN_EXPORT void openvpn_plugin_close_v1(openvpn_plugin_handle_t handle)
+openvpn_plugin_close_v1(openvpn_plugin_handle_t handle)
 {
     struct plugin_context *context = (struct plugin_context *) handle;
-    printf("FUNC: openvpn_plugin_close_v1\n");
+    free(context->script_path);
     free(context);
 }
+
