8fd0f54be2f75c9e3f6b3d4b71ad34713753547c
kawakami <44276443+Jnchi@users.noreply.github.com>
2019-07-01 15:26:31 -0400

    [skip ci] Update copyright

--- auth_aad.c
+++ auth_aad.c
@@ -6,6 +6,7 @@
  *             packet compression.
  *
  *  Copyright (C) 2002-2018 OpenVPN Inc <sales@openvpn.net>
+ *  Copyright (C) 2019 CyberNinjas <info@cyberninjas.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2
